87427814c711f26fdffb4b11f6f63eaa796bb73d
CPE3C-sandiego-denverlyndonaerol <2023101814@ms.bulsu.edu.ph>
2026-04-01 14:24:11 +0800

    Add routing to App component with Home and About pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 // This is the main App component. It will contain the routes of the websites
-import ApiTester from './assets/components/apitester'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home-page';
+import AboutPage from './pages/about-page';
+import ApiTester from './assets/components/apitester';
 
 function App() {
-  return <ApiTester />
+  return (
+    <BrowserRouter>
+      
+      <ApiTester /> 
+      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
